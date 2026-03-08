879f4b72ea3cfd686e8e676475c533fcd2b60d36
Thomas <thisisreede@gmail.com>
2026-03-08 11:13:59 +0100

    changed main to test profile corresponding to screen

--- crates/aura-cli/src/main.rs
+++ crates/aura-cli/src/main.rs
@@ -3,14 +3,17 @@ use aura_core::*;
 fn main() -> anyhow::Result<()> {
     let profile = ProfileBuilder::new(0, "Test Gears".to_string())
         .with_params(|p| {
-            // p.with_upscaler(|u| {
-            //     u.with_filter(gamescope::upscaler::Filter::Fsr)
-            //         .with_settings(gamescope::upscaler::Settings {
-            //             scale: Some(0.5),
-            //             sharpness: None,
-            //         })
-            // })
-            p.immediate_flips()
+            p.with_output_res((3440, 1440))
+                .with_nested_res((3440, 1440))
+                .with_refresh_rate(165)
+                .with_upscaler(|u| {
+                    u.with_filter(gamescope::upscaler::Filter::Fsr)
+                        .with_settings(gamescope::upscaler::Settings {
+                            scale: Some(0.5),
+                            sharpness: None,
+                        })
+                })
+                .immediate_flips()
         })
         .gamemode(true)
         .build();
